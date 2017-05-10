b1f4104690915e9ab8b8cb767d2d259a6f9b2269
Cauê Polimanti <caue.polimanti@gmail.com>
2017-05-10 19:15:56 -0300

    Nova localização adicionada

--- js/map.js
+++ js/map.js
@@ -1,33 +1,67 @@
-var map;
-    function initMap() {
-        var loc = {lat: -23.627683, lng: -46.639082};
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: loc,
-            zoom: 16
-        });
+var mapSaoJudas;
+var mapPaulista;
 
-        var contentString = '<div id="locContent">'+
-            '<div id="siteNotice">'+
-            '</div>'+
-            '<h2 id="firstHeading" class="firstHeading">Consultório</h1>'+
-            '<div id="bodyContent">'+
-            '<p>Rua Major Freire - Metrô São Judas' +
-            '</br>(Próximo à Av. Jabaquara) '+
-            '</br>São Paulo, SP 04304-110</p>'+
-            '</div>'+
-            '</div>';
+function initMap() {
+    var locSaoJudas = { lat: -23.627683, lng: -46.639082 };
+    mapSaoJudas = new google.maps.Map(document.getElementById('mapSaoJudas'), {
+        center: locSaoJudas,
+        zoom: 16
+    });
 
-        var infowindow = new google.maps.InfoWindow({
-            content: contentString
-        });
-        
-        var marker = new google.maps.Marker({
-            position: loc,
-            map: map,
-            title: 'Consultório'
-        });
-        marker.addListener('click', function() {
-            infowindow.open(map, marker);
-        });
-        //marker.setMap(map);
-    }
+    var contentStringSJ = '<div id="locContent">' +
+        '<div id="siteNotice">' +
+        '</div>' +
+        '<h2 id="firstHeading" class="firstHeading">Consultório</h1>' +
+        '<div id="bodyContent">' +
+        '<p>Rua Major Freire' +
+        '</br>(Próximo ao Metrô São Judas) ' +
+        '</br>São Paulo, SP 04304-110</p>' +
+        '</div>' +
+        '</div>';
+
+    var infowindowSJ = new google.maps.InfoWindow({
+        content: contentStringSJ
+    });
+
+    var markerSJ = new google.maps.Marker({
+        position: locSaoJudas,
+        map: mapSaoJudas,
+        title: 'Consultório'
+    });
+    markerSJ.addListener('click', function () {
+        infowindowSJ.open(mapSaoJudas, markerSJ);
+    });
+    //marker.setMap(map);
+
+
+    var locPaulista = { lat: -23.627683, lng: -46.639082 };
+    mapPaulista = new google.maps.Map(document.getElementById('mapPaulista'), {
+        center: locPaulista,
+        zoom: 16
+    });
+
+    var contentStringP = '<div id="locContent">' +
+        '<div id="siteNotice">' +
+        '</div>' +
+        '<h2 id="firstHeading" class="firstHeading">Consultório</h1>' +
+        '<div id="bodyContent">' +
+        '<p>Av. Paulista, 726' +
+        '</br>(Próximo ao Metrô Brigadeiro - Linha Verde) ' +
+        '</br>São Paulo, SP 01310-100</p>' +
+        '</div>' +
+        '</div>';
+
+    var infowindowP = new google.maps.InfoWindow({
+        content: contentStringP
+    });
+
+    var markerP = new google.maps.Marker({
+        position: locPaulista,
+        map: mapPaulista,
+        title: 'Consultório'
+    });
+    markerP.addListener('click', function () {
+        infowindowP.open(mapPaulista, markerP);
+    });
+    //marker.setMap(map);
+}
